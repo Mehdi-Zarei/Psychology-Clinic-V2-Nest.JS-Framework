d32f26ffe0c846dd2b824d36607aa80a00f3c1fb
Mehdi <mahdizareiofficial@gmail.com>
2025-10-30 18:25:36 +0330

    feat: enhance AvailableTimeController with CRUD operations and API documentation

--- src/available-time/available-time.controller.ts
+++ src/available-time/available-time.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Post, Body, Req, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+  Get,
+  Param,
+  ParseIntPipe,
+  Delete,
+  Patch,
+} from "@nestjs/common";
 import { AvailableTimeService } from "./available-time.service";
 import { CreateAvailableTimeDto } from "./dto/create-available-time.dto";
 import { Request } from "express";
-import { ApiBearerAuth, ApiConsumes } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiConsumes, ApiOperation } from "@nestjs/swagger";
 import { CustomAuthGuard } from "src/common/guards/auth.guard";
 import { Roles } from "src/common/decorators/roles.decorator";
 import { SwaggerConsumes } from "src/common/enums/SwaggerConsumes.enum";
+import { UpdateAvailableTimeDto } from "./dto/update-available-time.dto";
 
 @Controller("available-time")
 export class AvailableTimeController {
   constructor(private readonly availableTimeService: AvailableTimeService) {}
 
+  @ApiOperation({ summary: "Psychologist Can Create New Available Time." })
   @ApiBearerAuth("accessToken")
   @UseGuards(CustomAuthGuard)
   @Roles("ADMIN", "PSYCHOLOGIST")
@@ -21,6 +34,53 @@ export class AvailableTimeController {
     @Req() req: Request,
   ) {
     const userId = req.user.id;
-    return this.availableTimeService.create(createAvailableTimeDto, userId);
+    return this.availableTimeService.addFreeTime(
+      createAvailableTimeDto,
+      userId,
+    );
+  }
+
+  @ApiOperation({
+    summary: "Users can receive free times of a specific psychologist.",
+  })
+  @Get(":psychologistId")
+  getFreeTime(@Param("psychologistId", ParseIntPipe) id: number) {
+    return this.availableTimeService.getFreeTime(id);
+  }
+
+  @ApiOperation({
+    summary:
+      "The admin or psychologist who created the free time can delete it.",
+  })
+  @ApiBearerAuth("accessToken")
+  @UseGuards(CustomAuthGuard)
+  @Roles("ADMIN", "PSYCHOLOGIST")
+  @Delete(":id/remove")
+  removeTime(@Param("id", ParseIntPipe) id: number, @Req() req: Request) {
+    const userId = req.user.id;
+    return this.availableTimeService.removeTime(id, userId);
+  }
+
+  @ApiOperation({
+    summary:
+      "The admin or psychologist who created the free time can update it.",
+  })
+  @ApiBearerAuth("accessToken")
+  @UseGuards(CustomAuthGuard)
+  @Roles("ADMIN", "PSYCHOLOGIST")
+  @ApiConsumes(SwaggerConsumes.FORM)
+  @Patch(":id/update")
+  updateTime(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() updateAvailableTimeDto: UpdateAvailableTimeDto,
+    @Req()
+    req: Request,
+  ) {
+    const userId = req.user.id;
+    return this.availableTimeService.updateTime(
+      id,
+      userId,
+      updateAvailableTimeDto,
+    );
   }
 }
