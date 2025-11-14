d10acee6b0ef3dc25fed8594ff02b41ced3b9028
Mehdi <mahdizareiofficial@gmail.com>
2025-11-14 17:55:45 +0330

    Dynamic SMS sending service

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -4,33 +4,39 @@ import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class SmsService {
-  private readonly logger = new Logger(SmsService.name);
-
   constructor(private readonly httpService: HttpService) {}
 
-  async sendSms(userPhone: string, data: any): Promise<{ success: boolean }> {
+  async sendSms(phone: any, patternCode: any, variableName: any, value: any) {
     try {
       const response$ = this.httpService.post("http://ippanel.com/api/select", {
         op: "pattern",
-        user: process.env.OTP_USER,
-        pass: process.env.OTP_PASS,
+        user: process.env.SMS_USER,
+        pass: process.env.SMS_PASS,
         fromNum: "3000505",
-        toNum: userPhone,
-        patternCode: process.env.OTP_PATTERN,
-        inputData: [{ [process.env.OTP_VARIABLE!]: data }],
+        toNum: phone,
+        patternCode,
+        inputData: [
+          {
+            [variableName]: value,
+          },
+        ],
       });
 
       const response = await firstValueFrom(response$);
 
-      if (Array.isArray(response.data) || response.data.length > 1) {
-        this.logger.error("SMS Error Body -->", response.data);
-        return { success: false };
+      if (!response.data || Array.isArray(response.data)) {
+        throw new Error("SMS sending failed, ippanel response invalid");
       }
 
-      return { success: true };
+      if (typeof response.data === "number") {
+        return { success: true };
+      } else {
+        console.log("SMS Response Error -->", response.data);
+        throw new Error("SMS Response Error -->", response.data);
+      }
     } catch (error) {
-      this.logger.error("SMS Error -->", error);
-      return { success: false };
+      console.error("SMS Error:", error?.response?.data || error);
+      throw error;
     }
   }
 }
